a8ed5dfa068867689137be16ed4b642a718393bb
Hiba-Mohamed <hibamoha987@gmail.com>
2025-01-21 12:54:53 -0330

    matching node instead of bundler

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/InterProSync/", // Ensure this is set to your GitHub Pages repo name
-    build: {
-        outDir: "dist", // Make sure Vite is outputting to the correct directory
-        rollupOptions: {
-            input: "/src/main.tsx", // Ensure this points to the correct entry file
-        },
+    base: "/Collab-NAS-Project/",
+    optimizeDeps: {
+        include: ["*.ts", "*.d.ts", "*.tsx", "*.js", "*.jsx"],
     },
 });
